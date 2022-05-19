8659f185113ce012ec39d061f0b3374addc2c6db
Tatiana <tatnovid@gmail.com>
2022-05-19 16:08:08 +0300

    проверка на палиндром

---
+++ ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿// проверка на палиндром
+
+void Palindrom(int num)
+{    
+    if (num / 1000 == (num % 10) * 10 + (num % 100) / 10)
+     { 
+        Console.WriteLine("Это палиндром");
+     }
+    else
+    {
+        Console.WriteLine("Это не палиндром");  
+    }
+    return;
+}
+    Console.WriteLine("Введите пятизначное число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Palindrom(num);
